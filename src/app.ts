079f41da36df4dbf31f71bc05c60d62f3de16124
Ezen <82026690+ezengaston@users.noreply.github.com>
2022-09-15 00:15:07 +0300

    Added HasFormatter Interface and Payment class

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,6 @@
 import { Invoice } from "./classes/Invoice.js";
+import { Payment } from "./classes/Payment.js";
+import { HasFormatter } from "./interfaces/HasFormatter.js";
 
 const invOne = new Invoice("mario", "work on the mario website", 250);
 const invTwo = new Invoice("luigi", "work on the luigi website", 300);
--- src/classes/Payment.ts
+++ src/classes/Payment.ts
@@ -1,13 +1,13 @@
 import { HasFormatter } from "../interfaces/HasFormatter.js";
 
-export class Invoice implements HasFormatter {
+export class Payment implements HasFormatter {
   constructor(
-    private client: string,
+    private recipient: string,
     private details: string,
     private amount: number
   ) {}
 
   format() {
-    return `${this.client} owes £${this.amount} for ${this.details}`;
+    return `${this.recipient} is owed £${this.amount} for ${this.details}`;
   }
 }

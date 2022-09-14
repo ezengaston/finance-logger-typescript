1a96fa4c3bfa8ad833e5737611d998ebb67cbadd
Ezen <82026690+ezengaston@users.noreply.github.com>
2022-09-15 01:32:22 +0300

    Added ListTemplate class and ability to add items to the screen

--- public/app.js
+++ public/app.js
@@ -1,10 +1,13 @@
 import { Invoice } from "./classes/Invoice.js";
 import { Payment } from "./classes/Payment.js";
+import { ListTemplate } from "./classes/ListTemplate.js";
 const form = document.querySelector(".new-item-form");
 const type = document.querySelector("#type");
 const toFrom = document.querySelector("#tofrom");
 const details = document.querySelector("#details");
 const amount = document.querySelector("#amount");
+const ul = document.querySelector("ul");
+const list = new ListTemplate(ul);
 form.addEventListener("submit", (e) => {
     e.preventDefault();
     let doc;
@@ -14,5 +17,5 @@ form.addEventListener("submit", (e) => {
     else {
         doc = new Payment(toFrom.value, details.value, amount.valueAsNumber);
     }
-    console.log(doc);
+    list.render(doc, type.value, "end");
 });
--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import { Invoice } from "./classes/Invoice.js";
 import { Payment } from "./classes/Payment.js";
 import { HasFormatter } from "./interfaces/HasFormatter.js";
+import { ListTemplate } from "./classes/ListTemplate.js";
 
 const form = document.querySelector(".new-item-form") as HTMLFormElement;
 
@@ -8,6 +9,9 @@ const type = document.querySelector("#type") as HTMLSelectElement;
 const toFrom = document.querySelector("#tofrom") as HTMLInputElement;
 const details = document.querySelector("#details") as HTMLInputElement;
 const amount = document.querySelector("#amount") as HTMLInputElement;
+
+const ul = document.querySelector("ul")!;
+const list = new ListTemplate(ul);
 
 form.addEventListener("submit", (e: Event) => {
   e.preventDefault();
@@ -20,5 +24,5 @@ form.addEventListener("submit", (e: Event) => {
     doc = new Payment(toFrom.value, details.value, amount.valueAsNumber);
   }
 
-  console.log(doc);
+  list.render(doc, type.value, "end");
 });
